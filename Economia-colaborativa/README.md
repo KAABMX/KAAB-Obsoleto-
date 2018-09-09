02ea78adeb298f4d8abc2bf3c975ac2e5d11115c
luis <iluis@ciencias.unam.mx>
2018-09-08 22:17:39 -0400

    Prototipo del proyecto en Netbeans.

---
+++ Economia-colaborativa/README.md
@@ -0,0 +1,2 @@
+# Repaso
+Repaso de las tecnologias para el curso de # Ingenieria de SoftwareII 2019-1
---
+++ Economia-colaborativa/src/main/java/mx/unam/ciencias/is/controlador/ControladorIndex.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.unam.ciencias.is.controlador;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author jonh
+ */
+@Controller 
+public class ControladorIndex {
+    
+     /**
+      * Metodo que responde a la peticion raiz
+      * @return el nombre del jsp de inicio
+      */
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    
+    }
+    @RequestMapping(value="/peticion1", method = RequestMethod.GET)
+    public void peticion1(HttpServletRequest request){
+        String parametro = request.getParameter("parametro");
+        System.out.println("La peticion1 es: =================>"+parametro);
+    
+    }
+    
+    @RequestMapping(value="/peticion2", method = RequestMethod.GET)
+    public ModelAndView peticion2(HttpServletRequest request,ModelMap model){
+        String parametro = request.getParameter("parametro");
+        System.out.println("La peticion2 es: =================>"+parametro);
+        model.addAttribute("parametro", parametro);
+        return new ModelAndView("index",model);
+    
+    }
+    
+    @RequestMapping(value="/peticion3", method = RequestMethod.GET)
+    public ModelAndView peticion3(HttpServletRequest request,ModelMap model){
+        String parametro = request.getParameter("parametro");
+        System.out.println("La peticion2 es: =================>"+parametro);
+        model.addAttribute("parametro", parametro);
+        return new ModelAndView("index",model);
+    
+    }
+}
